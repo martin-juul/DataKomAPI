6865077b5d949c40c3d19f0e48bc557b59cf9037
Martin Juul <code@juul.xyz>
2018-03-23 01:29:31 +0100

    Formatted output
    Signed-off-by: Martin Juul <code@juul.xyz>

--- app/Repositories/SemesterRepository.php
+++ app/Repositories/SemesterRepository.php
@@ -9,6 +9,29 @@ use App\Models\Semesters;
 class SemesterRepository
 {
     public function getSemesterCoursesByEducation(int $studentTypeGroupId, int $educationId) {
-        return Semesters::getSemestersByGroupId($studentTypeGroupId, $educationId);
+        $items = Semesters::getSemestersByGroupId($studentTypeGroupId, $educationId);
+        $tree = [];
+
+        foreach ($items as $item) {
+            $semester = $item->semester;
+            $course_no = $item->course_no;
+            $name = $item->name;
+            $course_length = $item->course_length;
+            $school = $item->school_attendance_desc;
+            $internship = $item->internship_desc;
+
+            $tree[$semester]['semester'] = $semester;
+            $tree[$semester]['courses'][] = [
+                'course_no' => $course_no,
+                'name' => $name,
+                'length' => $course_length,
+                'school_attendance_desc' => $school,
+                'internship_desc' => $internship,
+            ];
+        }
+
+        // Removing semester from index.
+        // Effectively flattening the array by one dimension.
+        return array_values($tree);
     }
 }
