ad0622fb56c899d233d9682085829f6f7243149d
Martin Juul <code@juul.xyz>
2018-03-23 01:27:12 +0100

    [Models\Semesters] Fixed getSemestersByGroupId
    Signed-off-by: Martin Juul <code@juul.xyz>

--- app/Models/Semesters.php
+++ app/Models/Semesters.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Database\Query\JoinClause;
 
 class Semesters extends Model
 {
-    protected $table = 'semesters';
+    const TABLE_NAME = 'semesters';
+    protected $table = self::TABLE_NAME;
     protected $primaryKey = 'semester_id';
     protected $fillable = [
         'semester'
@@ -16,25 +18,45 @@ class Semesters extends Model
         'semester_id'
     ];
 
-    public static function getSemestersByGroupId(int $studentTypeGroupId, int $educationId) {
-        return DB::table('semesters')
-            ->join('courses_to_semesters', function ($join) {
-                $join->on('semesters.semester_id', '=', 'courses_to_semesters.semester_id');
-            })
-            ->join('courses', function ($join) {
-                $join->on('courses_to_semesters.course_id', '=', 'courses.id');
+    /**
+     * @param int $studentTypeGroupId
+     * @param int $educationId
+     * @return \Illuminate\Support\Collection
+     */
+    public static function getSemestersByGroupId(int $studentTypeGroupId, int $educationId)
+    {
+        return DB::table('student_type_groups')
+            ->join(self::TABLE_NAME,
+                'student_type_groups.id', '=', self::TABLE_NAME.'.student_type_group_id')
+            ->join('courses_to_semesters',
+                self::TABLE_NAME.'.semester_id', '=', 'courses_to_semesters.semester_id')
+            ->join('courses',
+                'courses_to_semesters.course_id', '=', 'courses.id')
+            ->join('course_length', function (JoinClause $join) {
+                $join->on('course_length.student_group_id', '=', 'student_type_groups.id');
+                $join->on('courses_to_semesters.course_id', '=', 'course_length.course_id');
+                $join->on('course_length.semester_id', '=', self::TABLE_NAME.'.semester_id');
             })
             ->get([
                 'semesters.student_type_group_id',
                 'semesters.education_id',
                 'semesters.semester',
                 'courses.course_no',
+                'course_length.course_length',
                 'courses.name',
                 'courses.school_attendance_desc',
-                'courses.internship_desc'
+                'courses.internship_desc',
             ])
             ->where('student_type_group_id', '=', $studentTypeGroupId)
             ->where('education_id', '=', $educationId);
     }
 
+    public static function GetColumns(string $columns)
+    {
+        $columns = explode('.', $columns);
+
+        return DB::table('semesters')
+            ->get($columns)
+            ->toArray();
+    }
 }
