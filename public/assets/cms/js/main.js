0d8e9531e770ec8ad25447b0cd802051dd7861b9
Martin Juul <code@juul.xyz>
2018-03-31 18:50:17 +0200

    [cms]: move cmsSendForm method to main.js
    Signed-off-by: Martin Juul <code@juul.xyz>

--- public/assets/cms/js/main.js
+++ public/assets/cms/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             "zeroRecords": "Ingen resultater",
             "info": "Viser side _PAGE_ af _PAGES_",
             "infoEmpty": "Ingen resultater",
-            "infoFiltered": "(filtreret af _MAX_ totale antal emner)",
+            "infoFiltered": "(filtreret af _MAX_ antal emner)",
             "decimal": ",",
             "thousands": ".",
             "paginate": {
@@ -16,3 +16,19 @@ $(document).ready(function() {
         }
     } );
 } );
+
+function cmsSendForm(event, url, form, modal) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: $(form).serialize(),
+        success: function () {
+            $(modal).modal('hide');
+            $(form)[0].reset();
+            location.reload(true);
+        }
+    });
+}
